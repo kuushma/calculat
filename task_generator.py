c940c30eb4d7531731859bf69853a67104d3d006
kuushma <mashakyvnareva@gmail.com>
2022-04-24 21:32:29 +0300

    commit

--- task_generator.py
+++ task_generator.py
@@ -1,8 +1,11 @@
-import sqlite3
+from PyQt5.QtWidgets import (QWidget, QSlider, QLineEdit, QLabel, QPushButton, QScrollArea,QApplication,
+                             QHBoxLayout, QButtonGroup, QRadioButton, QVBoxLayout, QMainWindow, QDoubleSpinBox)
+from PyQt5.QtCore import Qt, QSize
+from PyQt5 import QtWidgets, uic
+import sys
 import random
-
+import sqlite3
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
-from PyQt5 import uic
 from errorr.error import Error
 from errorr.unfaithful import Unfaithful
 from docx import Document
@@ -20,48 +23,46 @@ from PyQt5 import uic
 class task_generator(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('tasks.ui', self)
+        uic.loadUi('testss.ui', self)
         self.con = sqlite3.connect('Tesks.db')
         self.cur = self.con.cursor()
 
-        self.Run.clicked.connect(self.run)
+        self.widget = QWidget()
+        self.vbox = QVBoxLayout()
+        self.list_variables = []
         self.save.clicked.connect(self.Save)
-        self.back.clicked.connect(self.Back)
-        self.file.clicked.connect(self.File)
+        self.Run.clicked.connect(self.Finding_variables)
+        self.widget.setLayout(self.vbox)
+        self.parmenial.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.parmenial.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.parmenial.setWidgetResizable(True)
+        self.parmenial.setWidget(self.widget)
 
-        self.list_first = [self.label_3, self.label_4, self.first_min, self.first_max,
-                           self.first_whole, self.first_material]
-        for i in self.list_first:
-            i.hide()
         self.save.hide()
         self.file.hide()
+        self.file.clicked.connect(self.File)
 
-    def run(self):
+
+
+    def Finding_variables(self):
+        self.list_variables = []
+        self.variables = []
         task_text = self.tasks_text.text()
-        if task_text.count('&') == 0:
+        if task_text.count('&') == 0 or task_text.count('&') % 2 != 0:
             self.None_variable_error()
-        if task_text.count('&') % 2 != 0:
-            self.None_variable_error()
-        elif task_text.count('&') // 2 > 1:
-            self.Variables_error()
         else:
             self.lst_text_task = task_text.split()
             task_text = list(task_text)
-            self.lst_variables = []
             for i in range(task_text.count('&') // 2):
                 a1 = task_text.index('&')
                 del task_text[a1]
                 a2 = task_text.index('&')
                 del task_text[a2]
-                self.lst_variables.append(''.join(task_text[a1:a2]))
-            for i in self.lst_variables:
+                self.variables.append(''.join(task_text[a1:a2]))
+            for i in self.variables:
                 if len(i) == 0:
                     self.Emptiness_error()
-            for i in range(len(self.lst_variables)):
-                if i == 0:
-                    for j in self.list_first:
-                        j.show()
-                    self.first_variable.setText(self.lst_variables[i])
+                    # введите название переменной
             for i in range(len(self.lst_text_task)):
                 if self.lst_text_task[i].count('&') == 2:
                     a = list(self.lst_text_task[i])
@@ -71,12 +72,32 @@ class task_generator(QMainWindow):
             self.change = self.lst_text_task.copy()
             self.save.show()
 
-    def Save(self):
-        self.RadioButten()
-        self.Formula()
-        self.MinMax()
+            for i in range(len(self.variables)):
+                name_object = QLabel(self.variables[i])
+                self.vbox.addWidget(name_object)
+                min_object = QLabel('min')
+                self.vbox.addWidget(min_object)
+                self.min_p = QDoubleSpinBox()
+                self.min_p.setMinimum(-10000)
+                self.vbox.addWidget((self.min_p))
+                max_object = QLabel('max')
+                self.vbox.addWidget(max_object)
+                self.max_p = QDoubleSpinBox()
+                self.max_p.setMaximum(10000)
+                self.vbox.addWidget((self.max_p))
+                self.first_whole = QRadioButton('Целое')
+                self.first_material = QRadioButton('Вещественное')
+                lst_buttengroup = QButtonGroup()
+                lst_buttengroup.addButton(self.first_whole)
+                lst_buttengroup.addButton(self.first_material)
 
+                self.vbox.addWidget(self.first_whole)
+                self.vbox.addWidget(self.first_material)
+                # добавление всех данных для переменной
+                variables = [self.variables[i], self.min_p, self.max_p, lst_buttengroup, self.first_whole, self.first_material]
+                self.list_variables.append({i: variables})
 
+        self.save.show()
 
     def Formula(self):
         a = self.formula.text()
@@ -84,7 +105,7 @@ class task_generator(QMainWindow):
             self.None_formula_error()
         self.lst_formula = list(a.split())
         self.check = ['*', '/', '-', '+']
-        for i in self.lst_variables:
+        for i in self.variables:
             self.check.append(i)
             if i not in self.lst_formula:
                 self.Non_existent()
@@ -96,83 +117,83 @@ class task_generator(QMainWindow):
                     self.check.append(i)
         self.change_formula = self.lst_formula.copy()
 
-    def RadioButten(self):
-        count = 0
-        lst_buttengroup = [[self.first_whole, self.first_material]]
-        for i in range(len(self.lst_variables)):
-            a = lst_buttengroup[i]
-            for j in a:
-                if j.isChecked():
-                    count += 1
-            if count != 1:
-                self.Number_error()
-            else:
-                count = 0
-        self.sign = []
-        if len(self.lst_variables) == 0:
-            self.Number_error()
-        for i in range(len(self.lst_variables)):
-            a = lst_buttengroup[i]
-            if a[0].isChecked():
-                self.sign.append(True)
-            else:
-                self.sign.append(False)
 
-    def MinMax(self):
-        self.variables = []
-        lst_spinbox = [[self.first_min, self.first_max]]
+    def Save(self):
+        self.Formula()
+        self.variable_data = []
+        for i in range(len(self.variables)):
+            a = float(self.list_variables[i][i][1].value())
+            b = float(self.list_variables[i][i][2].value())
+            self.variable_data.append(a)
+            self.variable_data.append(b)
+            if self.list_variables[i][i][5].isChecked():
+                self.variable_data.append(1)
+            else:
+                self.variable_data.append(0)
+        self.Savee()
 
-        for i in range(len(self.lst_variables)):
-            a = lst_spinbox[i]
-            a1 = float(a[0].text().replace(',', '.'))
-            a2 = float(a[1].text().replace(',', '.'))
-            if a1 == a2 or a1 > a2:
-                self.Error_min_max()
-            elif len(self.lst_formula) == 0:
-                self.None_formula_error()
-            elif self.lst_variables[0] not in self.lst_formula:
-                self.No_variable()
-            elif len(set(self.check) & set(self.lst_formula)) != len(self.lst_formula):
+
+    def Savee(self):
+        a = self.formula.text()
+        if a == '':
+            self.None_formula_error()
+        self.lst_formula = list(a.split())
+        self.check = ['*', '/', '-', '+']
+        for i in self.variables:
+            self.check.append(i)
+            if i not in self.lst_formula:
                 self.Non_existent()
-            else:
-                self.cur.execute("""DELETE from tacks""")
-                self.variables.append([a1, a2])
-                self.a = int(self.spinBox.text())
+        for i in self.lst_formula:
+            if i not in self.check:
+                if not i.isdigit():
+                    self.Non_existent()
+                else:
+                    self.check.append(i)
+        self.change_formula = self.lst_formula.copy()
+        self.cur.execute("""DELETE from tacks""")
+        self.options = int(self.spinBox.text())
 
-                for i in range(self.a):
-                    for j in range(len(self.lst_variables)):
-                        self.Formula()
-                        if not self.variables:
-                            self.Error()
-                        else:
-                            b = self.variables[j][0]
-                            c = self.variables[j][1]
-                            if j == 0:
-                                if self.sign:
-                                    self.num1 = random.randint(int(b), int(c))
-                                else:
-                                    self.num1 = round(random.uniform(b, c), 3)
-                                self.lst_text_task[self.lst_text_task.index(self.lst_variables[0])] = str(self.num1)
-                                self.lst_formula[self.lst_formula.index(self.lst_variables[0])] = self.num1
+        a = self.formula.text()
+        aa = set(self.lst_text_task)
+        bb = set(self.lst_formula)
+        if a == '':
+            self.None_formula_error()
+        elif len(aa & bb) != len(self.variables):
+            self.Non_existent()
+        else:
+            for i in range(self.options):
+                if len(self.variables) * 3 < len(self.variable_data):
+                    self.Error()
+                for j in range(len(self.variables)):
+                    a = self.variable_data[j * 3]
+                    b = self.variable_data[1 + (j * 3)]
+                    if a > b or a == b:
+                        self.Error_min_max()
+                    if self.variable_data[2 + (j * 3)] == 0:
+                        self.num1 = random.randint(int(a), int(b))
+                    else:
+                        self.num1 = round(random.uniform(a, b), 2)
+                    self.lst_text_task[self.lst_text_task.index(self.variables[j])] = str(self.num1)
+                    self.lst_formula[self.lst_formula.index(self.variables[j])] = self.num1
 
-                        self.lst_text_task = list(map(str, self.lst_text_task))
-                        self.lst_formula = list(map(str, self.lst_formula))
-                        lst = [(' '.join(self.lst_text_task), str(eval(' '.join(self.lst_formula))))]
+                self.lst_text_task = list(map(str, self.lst_text_task))
+                self.lst_formula = list(map(str, self.lst_formula))
+                lst = [(' '.join(self.lst_text_task), str(eval(' '.join(self.lst_formula))))]
 
-                        self.cur.executemany("INSERT INTO tacks VALUES (?, ?)", lst)
-                        self.con.commit()
-                        self.lst_text_task = self.change.copy()
-                        self.lst_formula = self.change_formula.copy()
-                        self.file.show()
+                self.cur.executemany("INSERT INTO tacks VALUES (?, ?)", lst)
+                self.con.commit()
+                self.lst_text_task = self.change.copy()
+                self.lst_formula = self.change_formula.copy()
+                self.file.show()
 
     def File(self):
         a = self.cur.execute("""SELECT text FROM tacks""").fetchall()
         b = self.cur.execute("""SELECT ans FROM tacks""").fetchall()
         fname = QFileDialog.getSaveFileName()
         fname = fname[0]
-        for i in range(self.a + 1):
+        for i in range(self.options + 1):
             if i == 0:
-                for j in range(self.a):
+                for j in range(self.options):
                     if j == 0:
                         self.document = Document()
                         self.document.add_heading(f'Ответы', level=1)
